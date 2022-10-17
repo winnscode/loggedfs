381738c25a8f0dfa125d297eda2cdbd9f215b44f
Tao Wen <wentao@uniontech.com>
2022-10-17 10:46:09 +0800

    add the long file name (1024B) support

--- src/loggedfs.cpp
+++ src/loggedfs.cpp
@@ -35,9 +35,11 @@
 #include <dirent.h>
 #include <errno.h>
 #include <sys/statfs.h>
+
 #ifdef HAVE_SETXATTR
 #include <sys/xattr.h>
 #endif
+
 #include "easylogging++.h"
 #include <stdarg.h>
 #include <getopt.h>
@@ -47,6 +49,15 @@
 #include "Config.h"
 #include <stdexcept>
 #include <iostream>
+
+
+#include <stdio.h>
+#include <sstream>
+#include <cassert>
+#include <functional>
+#include "leveldb/db.h"
+
+
 
 INITIALIZE_EASYLOGGINGPP
 
@@ -72,6 +83,12 @@ static el::base::DispatchAction dispatchAction = el::base::DispatchAction::Norma
 static const char *loggerId = "default";
 static const char *additionalInfoFormat = " {%s} [ pid = %d %s uid = %d ]";
 static el::Logger *defaultLogger;
+
+
+static leveldb::DB* db;
+static leveldb::Status s;
+static string magic;
+static bool debugmode = false;
 
 const int MaxFuseArgs = 32;
 struct LoggedFS_Args
@@ -119,6 +136,204 @@ static char *getRelativePath(const char *path)
 
     return strdup(path);
 }
+
+
+// 文件名的转换
+// map方式随便写写的，216+16+16，可自定义，但转换后应该不大于255Byte
+string __map_name(string rawname)
+{
+        string n_str2;
+        std::hash<std::string> h;
+        size_t n = h(rawname);
+        stringstream ioss;
+        ioss << hex << n;
+        ioss >> n_str2;
+
+        string name2 = rawname.substr(0,216);
+        return name2 + magic + n_str2;
+}
+
+
+// 文件名转换的核心逻辑：
+// 使用了bool作为返回值，之后可以重构为直接返回空串，或者其它形式
+bool map_name(string rawname, string &realname)
+{
+        realname = "";
+        if (rawname.size() < 256) {
+                return false;
+        }
+
+        string tmp = __map_name(rawname);
+        string v;
+
+        // case1： 映射已经存在，直接返回结果
+        s = db->Get(leveldb::ReadOptions(), tmp, &v);
+
+        if (s.ok() && v == rawname) {
+                realname = tmp;
+                return true;
+        }
+
+        // case2： 映射不存在，创建并返回
+        if (s.IsNotFound()) {
+                s = db->Put(leveldb::WriteOptions(), tmp, rawname);
+                if (s.ok()) {
+                        realname = tmp;
+                        return true;
+                }
+        }
+
+        // case3：发生了冲突：realname作为key被其它rawname占用了，返回false，外层可以返回一个合适的错误值，表明该文件名不可用
+        // TODO： 更完整的冲突处理？
+        // 另外，现在是只要进入一次，rawname到realname的map关系就确定下来了，且这个map关系是该fuse全局的
+        return false;
+}
+
+
+
+
+// 将rawpath映射到realpath
+// 因为loggedfs的path处理是c，而leveldb的读写又是string，所以拼凑的时候也是混着用的。
+// realpath是要直接传递给local-fs的，走libc、vfs、fs的路径，所以要预先完整地做映射，
+// 所以需要在fuse里做路径解析，按"/"分割，并对其中的longname进行map。
+// 当前代码很乱，建议重写，现在可能有内存泄露风险或者逻辑bug之类的。
+char *map_path(const char *path, bool *ok)
+{
+    size_t len = strlen(path);
+    // 不管是否成功，orig作为遍历需要的临时字符串，会在函数内free，而dest需要调用者释放
+    char *orig = (char *)malloc(len+1);
+    char *dest = (char *)malloc(len+1);
+
+    // 原则上保持和orig path一样的格式，包括结尾的“/”
+    int need_tail = 0;
+
+    char *token;
+    char *seq;
+
+    strcpy(orig, path);
+
+    if (orig[len-1] == '/' && len != 1)
+        need_tail = 1;
+
+    // 因为后面都是用strcat修改dest，所以先将其构造为一个长度为0的字符串
+    // NOTE: 这是我临时了解c库的字符串处理而写的，使用方式很有可能有问题
+    dest[0] = '\0';
+
+    if (orig[0] == '/') {
+        strcat(dest, "/");
+        seq = orig + 1;
+    } else
+        seq = orig;
+
+    // 在orig上，用“/”做分割，一段一段处理
+    // strtok，我也不知道还有没有更好的方式……
+    token = strtok(seq, "/");
+    int first = 1;
+
+    while (token != NULL) {
+        // first时，不用加“/”
+        // 如果真的是以“/”开头，前面已经手动处理了
+        if (first)
+            first = 0;
+        else
+            strcat(dest, "/");
+
+        if (strlen(token) < 256) {
+            // 长度小于255时，不进入map路径
+            strcat(dest, token);
+        } else {
+            string src_str = token;
+            string map_str = "";
+            if (map_name(src_str, map_str)) {
+                strcat(dest, map_str.c_str());
+            } else {
+                free(orig);
+                *ok = 0;
+                return dest;
+            }
+        }
+        token = strtok(NULL, "/");
+    }
+
+    if (need_tail)
+        strcat(dest, "/");
+
+    free(orig);
+    *ok = 1;
+    return dest;
+}
+
+
+// 反向映射，从realname到rawname
+// 当不符合map规则，或者没有在数据库找到时，直接返回realname （注意！所以没有失败）
+string __map_reverse(string realname)
+{
+        if (realname.size() != 248) {
+                return realname;
+        }
+
+        // 现在的magic是临时生成的，可以固定写死
+        string tmp = realname.substr(216,16);
+        if (tmp != magic) {
+                cout << "tmp: " << tmp << endl;
+                cout << "magic: " << magic << endl;
+                return realname;
+        }
+
+        string v;
+        s = db->Get(leveldb::ReadOptions(), realname, &v);
+
+        // TODO：添加完整的访问错误逻辑处理
+        if (s.IsNotFound())
+                return realname;
+
+        return v;
+}
+
+
+// 做了char *的封装，没有考虑效率，需要优化
+char *map_reverse(const char *realname)
+{
+    string real = realname;
+    string raw = __map_reverse(real);
+
+    return strdup(raw.c_str());
+}
+
+
+
+// 核心是数据库的初始化
+// 目前是将db文件放在挂载点目录，后续可以：
+// 1 用.xxx.db隐藏
+// 2 进行额外的保护，比如防止删除、手动修改之类的
+bool map_init()
+{
+        std::hash<std::string> h;
+        size_t n2  = h("deepin mapfs!");
+        stringstream ioss;
+        ioss << hex << n2;
+        ioss >> magic;
+
+        cout << "magic: " << magic << endl;
+
+        // leveldb的部分可以参考网上的文档，我也是临时看，瞎用的……
+        leveldb::Options options;
+        options.create_if_missing = true;
+        s = leveldb::DB::Open(options, "./testdb", &db);
+        if (!s.ok()) {
+                if (debugmode)
+                    cerr << s.ToString() << endl;
+                else
+                    // ELPP_WRITE_LOG(el::base::Writer, el::Level::Info, dispatchAction, "default") << "map init failed." << endl;
+                    defaultLogger->info("Map init failed");
+
+                return false;
+        }
+        return true;
+}
+
+
+
 
 /*
  * Returns the name of the process which accessed the file system.
@@ -323,6 +538,7 @@ static int loggedFS_mknod(const char *orig_path, mode_t mode, dev_t rdev)
 
     return 0;
 }
+
 
 static int loggedFS_mkdir(const char *orig_path, mode_t mode)
 {
@@ -592,8 +808,9 @@ static int loggedFS_open(const char *orig_path, struct fuse_file_info *fi)
     return 0;
 }
 
-static int loggedFS_read(const char *orig_path, char *buf, size_t size, off_t offset,
-                         struct fuse_file_info *fi)
+
+static int loggedFS_read(const char *orig_path, char *buf, size_t size,
+    off_t offset, struct fuse_file_info *fi)
 {
     char *aPath = getAbsolutePath(orig_path);
     int res;
@@ -612,6 +829,8 @@ static int loggedFS_read(const char *orig_path, char *buf, size_t size, off_t of
     delete[] aPath;
     return res;
 }
+
+
 
 static int loggedFS_write(const char *orig_path, const char *buf, size_t size,
                           off_t offset, struct fuse_file_info *fi)
@@ -649,6 +868,8 @@ static int loggedFS_write(const char *orig_path, const char *buf, size_t size,
 
     return res;
 }
+
+
 
 static int loggedFS_statfs(const char *orig_path, struct statvfs *stbuf)
 {
@@ -726,6 +947,227 @@ static int loggedFS_removexattr(const char *orig_path, const char *name)
 }
 #endif /* HAVE_SETXATTR */
 
+
+
+
+// 通用模式是：加一层wrapper，预先做path的map
+// 大部分操作都是这样： 将用户传入的orig-path，转换为real-path，其它逻辑都不需要动
+// 我随手写了一部分，没有写全：比如access，rename，unlink之类的就还没写。
+// 可以考虑用宏来重构……
+// readdir()不同，它是读出目录中的real-name，转换为raw-name，并上报给fuse-driver
+static int loggedFS_getattr_2(const char *orig_path, struct stat *stbuf)
+{
+    bool ok;
+    int ret;
+    char *path = map_path(orig_path, &ok);
+
+    ret = 0;
+
+    // TODO：错误处理需要更加规范
+    if (!ok)
+        ret = -EINVAL;
+    else
+        ret = loggedFS_getattr(path, stbuf);
+
+out:
+    // 记得free
+    free(path);
+    return ret;
+}
+
+
+
+
+
+// readdir而言，两件事都要做：
+// 1. 将目录的orig-path，转换为real-path，往下传递
+// 2. 将下层返回的目录内文件的realname，根据需要反向映射为rawname，并填充dirent
+static int loggedFS_readdir_2(const char *orig_path, void *buf, fuse_fill_dir_t filler,
+                            off_t offset, struct fuse_file_info *fi)
+{
+    bool ok;
+    DIR *dp;
+    struct dirent *de;
+    int res;
+    char *aPath;
+    char *path;
+    char *real_path;
+
+    (void)offset;
+    (void)fi;
+
+
+
+    res = 0;
+    real_path = map_path(orig_path, &ok);
+
+    if (!ok) {
+        res = -EINVAL;
+        goto out2;
+    }
+
+
+    aPath = getAbsolutePath(real_path);
+    path = getRelativePath(real_path);
+
+
+
+    dp = opendir(path);
+    if (dp == NULL)
+    {
+        res = -errno;
+        loggedfs_log(aPath, "readdir", -1, "readdir %s", aPath);
+        goto out1;
+    }
+
+    while ((de = readdir(dp)) != NULL)
+    {
+        struct stat st;
+        memset(&st, 0, sizeof(st));
+        st.st_ino = de->d_ino;
+        st.st_mode = de->d_type << 12;
+
+        // 执行反向映射，注意free （不会失败）
+        char *r_name = map_reverse(de->d_name);
+
+        if (filler(buf, r_name, &st, 0)) {
+            free(r_name);
+            break;
+        } else {
+            free(r_name);
+        }
+
+    }
+
+    closedir(dp);
+    loggedfs_log(aPath, "readdir", 0, "readdir %s", aPath);
+
+    // 资源释放，按照内核的习惯用的goto，可以自行调整
+out1:
+    delete[] aPath;
+    free(path);
+
+out2:
+    free(real_path);
+    return res;
+}
+
+
+static int loggedFS_mknod_2(const char *orig_path, mode_t mode, dev_t rdev)
+{
+    bool ok;
+    int ret;
+    char *path = map_path(orig_path, &ok);
+
+    ret = 0;
+    if (!ok)
+        ret = -EINVAL;
+    else
+        ret = loggedFS_mknod(path, mode, rdev);
+
+out:
+    free(path);
+    return ret;
+}
+
+
+static int loggedFS_mkdir_2(const char *orig_path, mode_t mode)
+{
+    bool ok;
+    int ret;
+    char *path = map_path(orig_path, &ok);
+
+    ret = 0;
+    if (!ok)
+        ret = -EINVAL;
+    else
+        ret = loggedFS_mkdir(path, mode);
+
+out:
+    free(path);
+    return ret;
+}
+
+
+
+static int loggedFS_utimens_2(const char *orig_path,
+        const struct timespec ts[2])
+{
+    bool ok;
+    int ret;
+    char *path = map_path(orig_path, &ok);
+
+    ret = 0;
+    if (!ok)
+        ret = -EINVAL;
+    else
+        ret = loggedFS_utimens(path, ts);
+
+out:
+    free(path);
+    return ret;
+}
+
+
+
+static int loggedFS_open_2(const char *orig_path, struct fuse_file_info *fi)
+{
+    bool ok;
+    int ret;
+    char *path = map_path(orig_path, &ok);
+
+    ret = 0;
+    if (!ok)
+        ret = -EINVAL;
+    else
+        ret = loggedFS_open(path, fi);
+
+out:
+    free(path);
+    return ret;
+}
+
+
+
+static int loggedFS_read_2(const char *orig_path, char *buf, size_t size,
+    off_t offset, struct fuse_file_info *fi)
+{
+    bool ok;
+    int ret;
+    char *path = map_path(orig_path, &ok);
+
+    ret = 0;
+    if (!ok)
+        ret = -EINVAL;
+    else
+        ret = loggedFS_read(path, buf, size, offset, fi);
+
+out:
+    free(path);
+    return ret;
+}
+
+
+
+static int loggedFS_write_2(const char *orig_path, const char *buf, size_t size,
+                          off_t offset, struct fuse_file_info *fi)
+{
+    bool ok;
+    int ret;
+    char *path = map_path(orig_path, &ok);
+
+    ret = 0;
+    if (!ok)
+        ret = -EINVAL;
+    else
+        ret = loggedFS_write(path, buf, size, offset, fi);
+
+out:
+    free(path);
+    return ret;
+}
+
+
 static void usage(char *name)
 {
     fprintf(stderr, "Usage:\n");
@@ -767,6 +1209,9 @@ static bool processArgs(int argc, char *argv[], LoggedFS_Args *out)
 
 #define COMMON_OPTS "nonempty,use_ino,attr_timeout=0,entry_timeout=0,negative_timeout=0"
 
+    // Delete "nonempty" in fuse3
+// #define COMMON_OPTS "use_ino,attr_timeout=0,entry_timeout=0,negative_timeout=0"
+
     while ((res = getopt(argc, argv, "hpfec:l:")) != -1)
     {
         switch (res)
@@ -787,11 +1232,11 @@ static bool processArgs(int argc, char *argv[], LoggedFS_Args *out)
             got_p = true;
             defaultLogger->info("LoggedFS running as a public filesystem");
             break;
-        case 'e':
-            PUSHARG("-o");
-            PUSHARG("nonempty");
-            defaultLogger->info("Using existing directory");
-            break;
+        // case 'e':
+        //     PUSHARG("-o");
+        //     PUSHARG("nonempty");
+        //     defaultLogger->info("Using existing directory");
+        //     break;
         case 'c':
             out->configFilename = optarg;
             defaultLogger->info("Configuration file : %v", optarg);
@@ -858,7 +1303,6 @@ static bool processArgs(int argc, char *argv[], LoggedFS_Args *out)
 
 int main(int argc, char *argv[])
 {
-
     el::Configurations defaultConf;
     defaultConf.setToDefault();
     defaultConf.setGlobally(el::ConfigurationType::ToFile, std::string("false"));
@@ -874,12 +1318,12 @@ int main(int argc, char *argv[])
     // 0..
     memset(&loggedFS_oper, 0, sizeof(fuse_operations));
     loggedFS_oper.init = loggedFS_init;
-    loggedFS_oper.getattr = loggedFS_getattr;
+    loggedFS_oper.getattr = loggedFS_getattr_2;
     loggedFS_oper.access = loggedFS_access;
     loggedFS_oper.readlink = loggedFS_readlink;
-    loggedFS_oper.readdir = loggedFS_readdir;
-    loggedFS_oper.mknod = loggedFS_mknod;
-    loggedFS_oper.mkdir = loggedFS_mkdir;
+    loggedFS_oper.readdir = loggedFS_readdir_2;
+    loggedFS_oper.mknod = loggedFS_mknod_2;
+    loggedFS_oper.mkdir = loggedFS_mkdir_2;
     loggedFS_oper.symlink = loggedFS_symlink;
     loggedFS_oper.unlink = loggedFS_unlink;
     loggedFS_oper.rmdir = loggedFS_rmdir;
@@ -891,12 +1335,12 @@ int main(int argc, char *argv[])
 #if (FUSE_USE_VERSION == 25)
     loggedFS_oper.utime = loggedFS_utime;
 #else
-    loggedFS_oper.utimens = loggedFS_utimens;
+    loggedFS_oper.utimens = loggedFS_utimens_2;
     loggedFS_oper.flag_utime_omit_ok = 1;
 #endif
-    loggedFS_oper.open = loggedFS_open;
-    loggedFS_oper.read = loggedFS_read;
-    loggedFS_oper.write = loggedFS_write;
+    loggedFS_oper.open = loggedFS_open_2;
+    loggedFS_oper.read = loggedFS_read_2;
+    loggedFS_oper.write = loggedFS_write_2;
     loggedFS_oper.statfs = loggedFS_statfs;
     loggedFS_oper.release = loggedFS_release;
     loggedFS_oper.fsync = loggedFS_fsync;
@@ -909,6 +1353,7 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < MaxFuseArgs; ++i)
         loggedfsArgs->fuseArgv[i] = NULL; // libfuse expects null args..
+
 
     if (processArgs(argc, argv, loggedfsArgs))
     {
@@ -948,6 +1393,15 @@ int main(int argc, char *argv[])
         defaultLogger->info("chdir to %v", loggedfsArgs->mountPoint);
         chdir(loggedfsArgs->mountPoint);
         savefd = open(".", 0);
+
+
+
+    if (!map_init())
+        return -1;
+
+
+
+
 
 #if (FUSE_USE_VERSION == 25)
         fuse_main(loggedfsArgs->fuseArgc,
